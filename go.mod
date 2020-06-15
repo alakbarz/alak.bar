f9de010afbf9f9258da4371a7cd48011709450bd
Alakbar <me@alak.bar>
2020-06-15 23:08:55 +0100

    Version 0.9.6 + CSRF protection + Credits page + Credits link in footer - Comments in form of report.html > Text in footer
    Key
    + Added
    - Removed
    > Changed

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/go-macaron/binding v1.1.0
+	github.com/go-macaron/csrf v0.0.0-20200329073418-5d38f39de352 // indirect
+	github.com/yuin/goldmark v1.1.32 // indirect
 	gopkg.in/macaron.v1 v1.3.9
 )
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 
 	"github.com/go-macaron/binding"
+	"github.com/go-macaron/csrf"
+	"github.com/go-macaron/session"
 	"gopkg.in/macaron.v1"
 )
 
@@ -19,16 +21,18 @@ type reportForm struct {
 func main() {
 	m := macaron.Classic()
 	m.Use(macaron.Renderer())
+	m.Use(session.Sessioner())
+	m.Use(csrf.Csrfer())
 
 	m.Get("/", homeHandler)
 	m.Get("/projects", projectsHandler)
 	m.Get("/blog", blogHandler)
 	m.Get("/pics", picsHandler)
 	m.Get("/report", reportHandler)
+	m.Post("/report", csrf.Validate, binding.Bind(reportForm{}), reportHandlerPOST)
 	m.Get("/thankyou", thankyouHandler)
 	m.Get("/reporterror", reporterrorHandler)
-
-	m.Post("/report", binding.Bind(reportForm{}), reportHandlerPOST)
+	m.Get("/credits", creditsHandler)
 
 	log.Println("Server is running...")
 	log.Println(http.ListenAndServe("0.0.0.0:4000", m))
@@ -54,8 +58,9 @@ func picsHandler(ctx *macaron.Context) {
 	ctx.HTML(http.StatusOK, "pics")
 }
 
-func reportHandler(ctx *macaron.Context) {
+func reportHandler(ctx *macaron.Context, x csrf.CSRF) {
 	ctx.Data["Title"] = "Report Issue"
+	ctx.Data["csrf_token"] = x.GetToken()
 	ctx.HTML(http.StatusOK, "report")
 }
 
@@ -95,4 +100,9 @@ func reportHandlerPOST(ctx *macaron.Context, form reportForm) {
 		log.Print("Report email sent")
 		ctx.Redirect("/thankyou")
 	}
+}
+
+func creditsHandler(ctx *macaron.Context) {
+	ctx.Data["Title"] = "Credits"
+	ctx.HTML(http.StatusOK, "credits")
 }
